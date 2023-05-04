0c1e01468d271bacdd47f2a637b2c7bd19a72500
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-05-04 17:08:51 -0500

    make colors appear on the screen

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log('🟥 🟦 🟩 🟨');
+//activating jQuery
+$(document).ready(onReady);
+
+function onReady() {
+let redButton = $('red-btn');
+let blueButton = $('blue-btn');
+let greenButton = $('green-btn');
+let yellowButton = $('yellow-btn');
+
+$('#red-btn').on('click',redAppear);
+$('#blue-btn').on('click',blueAppear);
+$('#green-btn').on('click',greenAppear);
+$('#yellow-btn').on('click',yellowAppear);
+
+}
+function redAppear() {
+    $('#redContainer').append('<div class="block red-fill"></div>');
+}
+function blueAppear() {
+    $('#blueContainer').append('<div class="block blue-fill"></div>');
+}
+function greenAppear() {
+    $('#greenContainer').append('<div class="block green-fill"></div>');
+}
+function yellowAppear() {
+    $('#yellowContainer').append('<div class="block blue-fill"></div>');
+}
