dc0deff1579e8042251f9df3d457ba69bf249032
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-05-04 23:45:31 -0500

    created counter and one container that holds all

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 console.log('🟥 🟦 🟩 🟨');
 //activating jQuery
 $(document).ready(onReady);
-//defining buttons
+
+//defining color counts
+let redCounter = 1;
+let blueCounter = 1;
+let greenCounter = 1;
+let yellowCounter = 1;
+
+
 function onReady() {
+    //defining buttons
 let redButton = $('red-btn');
 let blueButton = $('blue-btn');
 let greenButton = $('green-btn');
 let yellowButton = $('yellow-btn');
+
 
 //call red button and when you click function redAppear happen
 $('#red-btn').on('click',redAppear);
@@ -14,41 +23,75 @@ $('#blue-btn').on('click',blueAppear);
 $('#green-btn').on('click',greenAppear);
 $('#yellow-btn').on('click',yellowAppear);
 
+//call the counters
+$('#red-btn').on('click',redCounter);
+$('#blue-btn').on('click',blueCounter);
+$('#green-btn').on('click',greenCounter);
+$('#yellow-btn').on('click',yellowCounter);
+
+
 //containers that we add second class too
-$('#redContainer').on('click','.red-fill',removeRed);
-$('#blueContainer').on('click','.blue-fill',removeBlue);
-$('#greenContainer').on('click','.green-fill',removeGreen);
-$('#yellowContainer').on('click','.yellow-fill',removeYellow);
+$('#boxContainer').on('click','.red-fill',removeRed);
+$('#boxContainer').on('click','.blue-fill',removeBlue);
+$('#boxContainer').on('click','.green-fill',removeGreen);
+$('#boxContainer').on('click','.yellow-fill',removeYellow);
 
+//containers that function go to
+// $('#redCounterContainer').on('click',redCount);
+// $('#blueCounterContainer').on('click',blueCount);
+// $('#greenCounterContainer').on('click',greenCount);
+// $('#yellowCounterContainer').on('click',yellowCount);
 
-
-}
-
+//functions that make boxes appear
+//and count
 function redAppear() {
-    $('#redContainer').append('<div class="block red-fill",></div>');
+    $('#boxContainer').append('<div class="block red-fill",></div>');
+    redCounter++;
+    $('#red-count').empty();
+     $('#red-count').append(redCounter);
+     
+
 }
 function blueAppear() {
-    $('#blueContainer').append('<div class="block blue-fill","red-fill"></div>');
+    $('#boxContainer').append('<div class="block blue-fill","red-fill"></div>');
+    blueCounter++
+    $('#blue-count').empty();
+    $('#blue-count').append(blueCounter);
+     
 }
 function greenAppear() {
-    $('#greenContainer').append('<div class="block green-fill"></div>');
+    $('#boxContainer').append('<div class="block green-fill"></div>');
+    greenCounter++
+    $('#green-count').empty();
+    $('#green-count').append(greenCounter);
 }
 function yellowAppear() {
-    $('#yellowContainer').append('<div class="block yellow-fill"></div>');
+    $('#boxContainer').append('<div class="block yellow-fill"></div>');
+    yellowCounter++
+    $('#yellow-count').empty();
+    $('#yellow-count').append(yellowCounter);
+    
 }
 
+}
+
+
+
+//functions that count
+
+
 /////////////Remove The Blocks Functions////////////////
-function removeRed(){
-    $(this).remove();
-}
-function removeBlue(){
-    $(this).remove();
-}
-function removeGreen(){
-    $(this).remove();
-}
-function removeYellow(){
-    $(this).remove();
-}
+// function removeRed(){
+//     $(this).remove();
+// }
+// function removeBlue(){
+//     $(this).remove();
+// }
+// function removeGreen(){
+//     $(this).remove();
+// }
+// function removeYellow(){
+//     $(this).remove();
+// }
 
 
